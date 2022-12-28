b4639e646423a33a0d857d002b13f3ebb51a279b
fznrsd@DESKTOP-DRD6N6C <fznrsd@DESKTOP-DRD6N6C.(MINGW64)git config --global user.name Faizan>
2022-12-28 14:53:57 +0530

    star rating system

--- pep/blogcomment/forms.py
+++ pep/blogcomment/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import BlogCommentModel
+from .models import BlogCommentModel, BlogRatingModel
 
 class BlogCommentForm(forms.ModelForm):
     class Meta:
         model = BlogCommentModel
         fields = ['comment']
+
+
+class BlogRatingForm(forms.ModelForm):
+    class Meta:
+        model = BlogRatingModel
+        fields = ['rating']
